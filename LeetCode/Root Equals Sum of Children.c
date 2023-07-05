0cfa7e3875e642d6a2914cd7d72aed8295e557e6
Yushirizu <113351099+Yushirizu@users.noreply.github.com>
2023-07-05 15:14:01 +0200

    :+1:

--- LeetCode/Root Equals Sum of Children.c
+++ LeetCode/Root Equals Sum of Children.c
@@ -1,11 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*Definition for a binary tree node.*/
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 bool checkTree(struct TreeNode *root)
 {
     if (root->val == (root->left->val) + (root->right->val))
---
+++ LeetCode/Running Sum of 1d Array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *runningSum(int *nums, int numsSize, int *returnSize)
+{
+    *returnSize = numsSize;
+
+    for (int i = 1; i < numsSize; i++)
+    {
+        nums[i] += nums[i - 1];
+    }
+
+    return nums;
+}
